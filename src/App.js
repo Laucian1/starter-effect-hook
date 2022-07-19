cb84c4c896681ded691939fdcf6e569ce59e1da6
Russell Smith <resmithjr88@gmail.com>
2022-07-18 20:46:03 -0600

    28.3 complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react"
 import ProfileEdit from './ProfileEdit';
 import './App.css';
 
 function App() {
+const [userID, setUserID] = useState(1)
+
+const userIds = [1,2,3,4]
+
   return (
-    <ProfileEdit />
+    <div className="App">
+      {userIds.map((id) => (
+        <button key={id} onClick={() => setUserID(id)}>
+          User ID {id}
+        </button>
+      ))}
+      <h2>User ID {userID}</h2>
+      <ProfileEdit userID={userID} />
+    </div>
   );
 }
 
--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -1,21 +1,36 @@
 import React, {useState, useEffect} from "react"
 
-function ProfileEdit() {
+function ProfileEdit({ userID }) {
     const [user, setUser] = useState({})
 
-    console.log("render", user)
     useEffect (() => {
-        console.log("useEffect")
-        async function loadUsers() {
-            const response = await fetch(
-                "https://jsonplaceholder.typicode.com/users/1"
-            )
-            const userFromAPI = await response.json()
-            console.log("setUser", userFromAPI)
-            setUser(userFromAPI)
+        setUser({})
+        const abortController = new AbortController()
+
+        async function loadUser() {
+            try {
+                const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users/${userID}`,
+                    { signal: abortController.signal }
+                )
+                const userFromAPI = await response.json()
+                setUser(userFromAPI)
+            } catch (error) {
+                if (error.name === "AbortError") {
+                    console.log("Aborted", userID)
+                } else {
+                    throw error
+                }
+            }
         }
-        loadUsers()
-    }, [])
+
+        loadUser()
+
+        return () => {
+            console.log("cleanup", userID)
+            abortController.abort()
+        }
+    }, [userID])
 
     useEffect(() => {
         if (user.username) {
@@ -25,7 +40,7 @@ function ProfileEdit() {
         }
     }, [user])
 
-    const changeHandler = event => {
+    const changeHandler = (event) => {
         setUser({ ...user, [event.target.name]: event.target.value })
     }
 
@@ -45,29 +60,32 @@ function ProfileEdit() {
     if (user.id) {
         return (
             <form name="profileEdit" onSubmit={submitHandler}>
-                <div>
-                    <label htmlFor="username">User Name:</label>
-                    <input
-                        id="username"
-                        name="username"
-                        type="text"
-                        required={true}
-                        onChange={changeHandler}
-                        value={user.username}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input 
-                        id="email" 
-                        name="email" 
-                        type="email" 
-                        required={true}
-                        value={user.email} 
-                        onChange={changeHandler}
-                    />
-                </div>
-                <button type="submit">Save</button>
+                <fieldset>  
+                    <legend>API User ID: {user.id}</legend>
+                    <div>
+                        <label htmlFor="username">User Name:</label>
+                        <input
+                            id="username"
+                            name="username"
+                            type="text"
+                            required={true}
+                            onChange={changeHandler}
+                            value={user.username}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input 
+                            id="email" 
+                            name="email" 
+                            type="email" 
+                            required={true}
+                            value={user.email} 
+                            onChange={changeHandler}
+                        />
+                    </div>
+                    <button type="submit">Save</button>
+                </fieldset>
             </form>
         )
     }
